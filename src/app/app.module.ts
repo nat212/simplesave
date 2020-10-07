c9df2078acf2ed91426e0ba6886aac9870b78ed0
Natasha Draper <natasha@draper.net.za>
2020-10-07 20:22:46 +0200

    Add breadcrumbs bar + add paths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { SharedModule } from '@modules/shared/shared.module';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
-import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
-import { environment } from '../environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BreadcrumbsBarComponent } from './components/breadcrumbs-bar/breadcrumbs-bar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BreadcrumbsBarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     environment.production ? [] : AkitaNgDevtools.forRoot(),
     AkitaNgRouterStoreModule,
     BrowserAnimationsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/breadcrumbs-bar/breadcrumbs-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ss-breadcrumbs-bar',
+  templateUrl: './breadcrumbs-bar.component.html',
+  styleUrls: ['./breadcrumbs-bar.component.scss'],
+})
+export class BreadcrumbsBarComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
