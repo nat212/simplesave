4ba46ce13509f6b3352673c837a4ac1a35156b19
Natasha Draper <natasha@quantsolutions.co.za>
2020-10-07 16:15:26 +0200

    Add auth/goal paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'session', loadChildren: () => import('./modules/session/session.module').then((m) => m.SessionModule) },
+  { path: 'goals', loadChildren: () => import('./modules/goal/goal.module').then((m) => m.GoalModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
