08ce8d215ffb2c7ea008314428798d28bc0eda42
Natasha Draper <natasha@draper.net.za>
2020-10-07 20:27:01 +0200

    Setup basic structure of breadcrumbs bar

--- src/app/components/breadcrumbs-bar/breadcrumbs-bar.component.ts
+++ src/app/components/breadcrumbs-bar/breadcrumbs-bar.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterQuery } from '@datorama/akita-ng-router-store';
+import { Observable, of } from 'rxjs';
+
+export interface Crumb {
+  path: string | string[];
+  label: string;
+}
 
 @Component({
   selector: 'ss-breadcrumbs-bar',
@@ -6,7 +13,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./breadcrumbs-bar.component.scss'],
 })
 export class BreadcrumbsBarComponent implements OnInit {
-  constructor() {}
+  public crumbs$: Observable<Crumb[]>;
+  constructor(private routerQuery: RouterQuery) {}
 
-  ngOnInit(): void {}
+  public ngOnInit(): void {
+    this.crumbs$ = of([]);
+    this.routerQuery.selectData().subscribe(console.log);
+  }
 }
