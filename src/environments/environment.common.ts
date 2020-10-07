c2a9e1e75999f3710be7eaebf4fd5409f0e1ada6
Natasha Draper <natasha@quantsolutions.co.za>
2020-10-07 13:10:05 +0200

    Add firebase config

---
+++ src/environments/environment.common.ts
@@ -0,0 +1,12 @@
+export const commonEnvironment = {
+  firebase: {
+    appId: '1:922873981419:web:83d8ffd77a2f5b5e3bd1ae',
+    apiKey: 'AIzaSyD_Y2MMvTTgKIURe8ZfvPDt0rPqAB46DjA',
+    authDomain: 'simple-save-2.firebaseapp.com',
+    databaseURL: 'https://simple-save-2.firebaseio.com',
+    messagingSenderId: '922873981419',
+    projectId: 'simple-save-2',
+    storageBucket: 'simple-save-2.appspot.com',
+    measurementId: 'G-HC0X3GV9GG',
+  },
+};
